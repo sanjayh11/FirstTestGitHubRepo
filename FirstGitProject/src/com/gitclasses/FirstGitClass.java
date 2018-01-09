532b79b3b946ec8d213bd1a08b6c96a5b39231ec
sanja <sanja@192.168.1.3>
2018-01-09 11:18:46 +0530

    Commit 1 by SH

---
+++ FirstGitProject/src/com/gitclasses/FirstGitClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.gitclasses;
+
+/**
+ * @author sanja
+ *
+ */
+public class FirstGitClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
